4f21c17cd6ea3273802837ebc3a2e15b11fdb50b
Leah Heil <leah@casaheil.nl>
2021-04-21 21:12:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tostiorderappstart/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.tostiorderappstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView orderText = (TextView) findViewById(R.id.orderText);
+        TextView enterNameText = (TextView) findViewById(R.id.enterNameText);
+        TextView howManyText = (TextView) findViewById(R.id.howManyText);
+        TextView seekBarText = (TextView) findViewById(R.id.seekBarText);
+        Button orderBtn = (Button) findViewById(R.id.orderBtn);
+        TextInputLayout nameField = (TextInputLayout) findViewById(R.id.nameField);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        CheckBox checkBoxHam = (CheckBox) findViewById(R.id.checkBoxHam);
+        CheckBox checkBoxCheese = (CheckBox) findViewById(R.id.checkBoxCheese);
+        CheckBox checkBoxHam2 = (CheckBox) findViewById(R.id.checkBoxHam2);
+        CheckBox checkBoxCheese2 = (CheckBox) findViewById(R.id.checkBoxCheese2);
+        CheckBox checkBoxHam3 = (CheckBox) findViewById(R.id.checkBoxHam3);
+        CheckBox checkBoxCheese3 = (CheckBox) findViewById(R.id.checkBoxCheese3);
+        TableRow row1 = (TableRow) findViewById(R.id.row1);
+        TableRow row2 = (TableRow) findViewById(R.id.row2);
+        TableRow row3 = (TableRow) findViewById(R.id.row3);
+
+        // default is 1 tosti so the second and third row should be gone
+        row2.setVisibility(View.GONE);
+        row3.setVisibility(View.GONE);
+
+
+
+        // programming of the order button
+        orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Payment.class);
+
+                // get all values from this page
+                String name = nameField.getEditText().getText().toString();
+                Integer amount = seekBar.getProgress() + 1;
+                Boolean ham = checkBoxHam.isChecked();
+                Boolean cheese = checkBoxCheese.isChecked();
+
+                // send the values to the payment page
+                i.putExtra("name", name);
+                i.putExtra("amount", amount);
+                i.putExtra("ham", ham);
+                i.putExtra("cheese", cheese);
+
+                startActivity(i);
+            }
+        });
+
+        // showing the value of the seekbar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // set the value under the seekbar to the value of the seekbar (+1 because the seekbar starts at 0)
+                seekBarText.setText(String.valueOf(progress+1));
+
+                // add and remove rows for choosing ham and/or cheese for each tosti
+                if(progress+1 == 1) {
+                    row1.setVisibility(View.VISIBLE);
+                    row2.setVisibility(View.GONE);
+                    row3.setVisibility(View.GONE);
+                } else if (progress+1 == 2) {
+                    row1.setVisibility(View.VISIBLE);
+                    row2.setVisibility(View.VISIBLE);
+                    row3.setVisibility(View.GONE);
+                } else if (progress+1 == 3) {
+                    row1.setVisibility(View.VISIBLE);
+                    row2.setVisibility(View.VISIBLE);
+                    row3.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tostiorderappstart/Payment.java
@@ -0,0 +1,69 @@
+package com.example.tostiorderappstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        TextView nameText = (TextView) findViewById(R.id.paymentName);
+        Button payNowBtn = (Button) findViewById(R.id.payNowBtn);
+        Button payLaterBtn = (Button) findViewById(R.id.payLaterBtn);
+
+        Bundle extras = getIntent().getExtras();
+        String name;
+        Integer amount;
+        Boolean ham;
+        Boolean cheese;
+
+
+        // show the order
+        if (extras != null) {
+            name = extras.getString("name");
+            amount = extras.getInt("amount");
+            ham = extras.getBoolean("ham");
+            cheese = extras.getBoolean("cheese");
+            if (ham && cheese) {
+                nameText.setText(Html.fromHtml("You have ordered <font color=#E62272>" + amount.toString() +
+                        "</font> tosti(s) with <font color=#E62272>ham and cheese</font>, using the name <font color=#E62272>" + name + "</font>"));
+            } else if (ham) {
+                nameText.setText(Html.fromHtml("You have ordered <font color=#E62272>" + amount.toString() +
+                        "</font> tosti(s) with <font color=#E62272>ham</font>, using the name <font color=#E62272>" + name + "</font>"));
+            } else if (cheese) {
+                nameText.setText(Html.fromHtml("You have ordered <font color=#E62272>" + amount.toString() +
+                        "</font> tosti(s) with <font color=#E62272>cheese</font>, using the name <font color=#E62272>" + name + "</font>"));
+            } else {
+                nameText.setText("You have ordered just toasted bread, please try again ;)");
+            }
+        }
+
+
+        // pay now
+        payNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Payment.this, PaymentComplete.class);
+                startActivity(i);
+            }
+        });
+
+        // pay later
+        payLaterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Payment.this, PaymentComplete.class);
+                startActivity(i);
+            }
+        });
+    }
+}
